7b784772e88c7651f6dc4cba3f4046850163360c
szsdk <shenz34206@hotmail.com>
2024-05-04 09:33:09 +0200

    add: pipeline test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -32,3 +32,25 @@ def test_tools(tmp_path, testing_jhcfg):
     assert fn.read_text() == "123\n"
     cli.tools.log_message("hello")
     cli.tools.compress_log(0)
+
+
+def test_pipeline(tmp_path, monkeypatch, slurm_server):
+    monkeypatch.syspath_prepend(tmp_path)
+    monkeypatch.chdir(tmp_path)
+
+    print(tmp_path)
+    subprocess.run(
+        """
+jh init
+git init
+git add .
+git commit -m "init"
+jh add-one -n 2 - run
+jh project from-config project.yaml run --nodry
+jh project-result from-config log/project/1.json - job_states
+jh project-result from-config log/project/1.json - job_states -o tt.html
+    """,
+        shell=True,
+        cwd=tmp_path,
+        check=True,
+    )
